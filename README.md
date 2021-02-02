22034ec51765c618461730c8f55197ee8c060cf2
Mohammed Sohail <sohailsameja@gmail.com>
2021-02-02 22:16:37 +0300

    release: v0.0.1

---
+++ README.md
@@ -0,0 +1,23 @@
+# administrative-divisions-db
+
+> Administrative divisons of countries as a JSON API over Github
+
+## Why?
+
+The current alternatives are unreliable:
+ * Rate limited
+ * No uptime gurantee
+ * Require API key and have a quota
+
+## About
+
+This repo automagically serves an "API" over GitCDN/Rawgit (Cloudflare network). No limits and uptime is guranteed. To get the administrative division of a particular country, simply make a GET request to either:
+
+```
+```
+
+Data is sourced from [GeoNames](http://www.geonames.org/).
+
+## License
+
+The Unlicense
---
+++ parser.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const _ = require("lodash");
+
+async function main() {
+  try {
+    const extractedData = [];
+    const countriesData = [];
+    const processedData = {};
+
+    const rawData = await fs.promises.readFile("./raw.txt", "utf8");
+    const rawLines = rawData.split("\n");
+
+    for (const i of rawLines) {
+      const lineData = i.split("\t");
+
+      const countryCode = i.split(".")[0];
+      const adminRegion = lineData[1];
+
+      countriesData.push(countryCode);
+
+      extractedData.push({
+        c: countryCode,
+        a: adminRegion,
+      });
+    }
+
+    for (const i of _.uniq(countriesData)) {
+      processedData[i] = [];
+    }
+
+    for (const i of extractedData) {
+      processedData[i.c].push(i.a);
+    }
+
+    for (const country in processedData) {
+      await fs.promises.writeFile(
+        `./api/${country}.json`,
+        JSON.stringify(processedData[country])
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
